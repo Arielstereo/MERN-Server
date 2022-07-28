8e489d4802f7a944e441753734020eeaf3a28e9b
Ariel Martinez <arielstereo@msn.com>
2022-07-28 15:53:28 -0300

    cors middleware

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 4000;
 const app = express();
 
 //middlewares
-app.use(cors());
+
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
@@ -19,6 +19,7 @@ app.use((req, res, next) => {
   );
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, 	DELETE");
   res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+  app.use(cors());
   next();
 });
 app.use(cookieParser());
