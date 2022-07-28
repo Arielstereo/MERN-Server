90e4662bbc3ff988b2717314d5f2ed439be75f3c
Ariel Martinez <arielstereo@msn.com>
2022-07-28 16:16:19 -0300

    cors

--- index.js
+++ index.js
@@ -11,21 +11,11 @@ const app = express();
 
 //middlewares
 
-const whitelist = ["http://localhost:3000"];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(cookieParser());
 app.use(express.json());
 app.use(morgan("dev"));
