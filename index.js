3105fd2d01ea2d3d46611d98d77c771ed0f3c9bd
Ariel Martinez <arielstereo@msn.com>
2022-07-28 16:34:01 -0300

    cors in vercel.json

--- index.js
+++ index.js
@@ -11,11 +11,14 @@ const app = express();
 
 //middlewares
 
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  app.use(cors());
+  next();
+});
 app.use(cookieParser());
 app.use(express.json());
 app.use(morgan("dev"));
