bc7f992e7db1e651c49d643e77a86bc4caa3678f
Ariel Martinez <arielstereo@msn.com>
2022-07-28 14:36:23 -0300

    cors configuration

--- index.js
+++ index.js
@@ -13,12 +13,14 @@ const PORT = process.env.PORT || 4000;
 const app = express();
 
 //middlewares
-app.use(
-  cors(
-    (origin = "localhost:3000"), //servidor que consume o (*) en caso que sea acceso libre
-    (credentials = true)
-  )
-);
+app.use(cors());
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, 	X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-	Method');
+	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, 	DELETE');
+	res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+	next();
+});
 app.use(cookieParser());
 app.use(express.json());
 app.use(morgan("dev"));
