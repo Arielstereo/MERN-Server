bc231f36cc17d69af83e6e64ed3fc0a2dfd87477
Ariel Martinez <arielstereo@msn.com>
2022-07-29 16:31:57 -0300

    change headers

--- index.js
+++ index.js
@@ -13,13 +13,12 @@ const app = express();
 app.use(cors());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Authorization,X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
   );
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  res.header("Access-Control-Allow-Credentials", true);
   next();
 });
 app.use(cookieParser());
