520dde8ab72f399bf1705e2c92a7726c2e97a68d
Ariel Martinez <arielstereo@msn.com>
2022-07-29 14:46:59 -0300

    fix headers index.js

--- index.js
+++ index.js
@@ -13,14 +13,12 @@ const app = express();
 app.use(cors());
 
 app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "*"
-  );
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
-    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method, multipart/form-data"
+    "Authorization, X-API-KEY, Origin, X-Requested-With, Accept, Access-Control-Allow-Request-Method"
   );
+  res.header("Content-Type", "multipart/form-data");
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
   next();
